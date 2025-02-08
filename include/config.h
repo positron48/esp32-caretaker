65b6597fb1942d1ed6511992eefad98f9f80025d
Filatov Anton <filatov@intaro.ru>
2025-02-08 21:55:00 +0300

    параметры конфига

--- include/config.h
+++ include/config.h
@@ -7,18 +7,18 @@
  * FRAMESIZE_SXGA - 1280x1024
  * FRAMESIZE_UXGA - 1600x1200
  */
-#define CAMERA_FRAMESIZE FRAMESIZE_SVGA
+#define CAMERA_FRAMESIZE FRAMESIZE_QVGA
 #define CAMERA_JPEG_QUALITY 12 // 0-63 (lower means higher quality)
 #define CAMERA_XCLK_FREQ 20000000 // 20MHz
 #define CAMERA_PIXEL_FORMAT PIXFORMAT_JPEG
 #define CAMERA_FB_COUNT 2 // Frame buffer count
 
 #define STR_HDR_BUF_LEN 64
-#define STR_MAX_FRAME_SIZE 32 * 1024 // Уменьшаем максимальный размер кадра до 32KB
-#define STR_STREAM_TASK_STACK_SIZE 4096
-#define STR_STREAM_TASK_PRIORITY 1
-#define STR_STREAM_TASK_CORE 1
-#define STR_STREAM_DELAY_MS 10 // Уменьшаем задержку между кадрами
+#define STR_MAX_FRAME_SIZE 64 * 1024 // Увеличиваем максимальный размер кадра до 64KB
+#define STR_STREAM_TASK_STACK_SIZE 8192 // Увеличиваем размер стека
+#define STR_STREAM_TASK_PRIORITY 2
+#define STR_STREAM_TASK_CORE 0 // Переносим на ядро 0
+#define STR_STREAM_DELAY_MS 20 // Небольшая задержка для стабильности
 
 // Stream content type
 #define STR_PART_BOUNDARY "123456789000000000000987654321"
